75ea8b4bf890af620336ae6f84d385aaffe0b981
htsuka <h2kazawa@gmail.com>
2023-07-08 23:59:56 +0900

    seed作成

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            ['username' => 'Atlas1',
+             'mail'     => 'Atlas1@gmail.com',
+             'password' => 'atAtlas01'],
+             ['username' => 'Atlas2',
+             'mail'     => 'Atlas2@gmail.com',
+             'password' => 'atAtlas02'],
+             ['username' => 'Atlas3',
+             'mail'     => 'Atlas3@gmail.com',
+             'password' => 'atAtlas03'],
+        ]);
     }
 }
